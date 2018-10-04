118d9b26ad1fec672e924c8ec28a0cdd8bf8f452
anshuman1001 <43641190+anshuman1001@users.noreply.github.com>
2018-10-04 14:38:32 +0530

    Add files via upload

---
+++ QNode.java
@@ -0,0 +1,16 @@
+
+
+public class QNode {
+	int x;
+	int y;
+	int distance;
+	int h;
+	QNode(int x,int y, int distance,int h)
+	{
+		this.x=x;
+		this.y=y;
+		this.distance=distance;
+		this.h = h;
+	}
+	
+}
